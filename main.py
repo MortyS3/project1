094169fb0de7c20199ee8df26f9f32962fa393df
Andrey <andreypedko99@gmail.com>
2020-02-19 17:41:36 +0200

    initial

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/first')
 def hello():
     name = request.args.get("name", "World")
     return f'Andrey Pedko, TZ-61'
+def function():
+    name = request.args.get("age", "country", "classroom")
+    return f'age, country, classroom' 
+    pass
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
